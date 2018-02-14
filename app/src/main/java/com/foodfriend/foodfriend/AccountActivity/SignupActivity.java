d84fcab9b26897bb40dbf46ee51ea83740d11941
Dan Westaway <dan.westaway@students.plymouth.ac.uk>
2018-02-14 15:13:29 +0000

    Screen layouts
    Created basic Ui for screens: login, sign up, password reset, password change

---
+++ app/src/main/java/com/foodfriend/foodfriend/AccountActivity/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.foodfriend.foodfriend.AccountActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.foodfriend.foodfriend.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
