3d893bbdfd5a7d1f9e8644b0fa30743ff0818756
Dan Westaway <dan.westaway@students.plymouth.ac.uk>
2018-03-15 16:45:19 +0000

    Messages database
    Recieving chat messages from database and displaying them with RecyclerView

--- app/src/main/java/com/foodfriend/foodfriend/ChatActivity.java
+++ app/src/main/java/com/foodfriend/foodfriend/ChatActivity.java
@@ -2,10 +2,14 @@ package com.foodfriend.foodfriend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -15,6 +19,7 @@ public class ChatActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
 
+    private RecyclerView messageList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,29 @@ public class ChatActivity extends AppCompatActivity {
         editMessage = findViewById(R.id.sendMessage);
 
         mDatabase = FirebaseDatabase.getInstance().getReference().child("messages");
+
+        messageList = (RecyclerView) findViewById(R.id.messageRecieved);
+
+        messageList.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true); //start displaying messages from the bottom
+
+        messageList.setLayoutManager(linearLayoutManager); //set the layout manager for the message list
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //use firebase's RecyclerAdapter, parameters: Message class, message layout, message view holder and database reference
+        FirebaseRecyclerAdapter <Message,MessageViewHolder> firebaseRec = new FirebaseRecyclerAdapter<Message, MessageViewHolder>(Message.class,R.layout.message,MessageViewHolder.class, mDatabase) {
+            @Override
+            protected void populateViewHolder(MessageViewHolder viewHolder, Message model, int position) {
+                viewHolder.setContent(model.getContent());
+            }
+        };
+        messageList.setAdapter(firebaseRec);
     }
 
     public void buttonClicked(View view) {
@@ -38,7 +66,23 @@ public class ChatActivity extends AppCompatActivity {
             final DatabaseReference ref = mDatabase.push();
 
             //send message to server
-            ref.child("message").setValue(message);
+            ref.child("content").setValue(message);
+        }
+    }
+
+    public static class MessageViewHolder extends RecyclerView.ViewHolder {
+        View view;
+
+        public MessageViewHolder(View itemView) {
+            super(itemView);
+            view = itemView;
+        }
+
+        public void setContent(String content) {
+            TextView chatMessage = view.findViewById(R.id.messageText); //get reference to users message from message.xml
+
+            chatMessage.setText(content);
+
         }
     }
 }
---
+++ app/src/main/java/com/foodfriend/foodfriend/Message.java
@@ -0,0 +1,25 @@
+package com.foodfriend.foodfriend;
+
+/**
+ * Created by Dan on 15/03/2018.
+ */
+
+public class Message {
+
+    private String content;
+
+    public Message() {
+
+    }
+    public Message(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
